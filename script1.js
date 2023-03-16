327dfbaa595a1c0e9d2486dbb312c3462ff6127a
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-16 19:41:43 +0400

    pagination (not done yet)

---
+++ script1.js
@@ -0,0 +1,30 @@
+const firstSection = document.querySelector(".cards").children;
+// console.log(firstSection)
+const prev =  document.querySelector(".prev");
+const next =  document.querySelector(".prev");
+const maxItem = 12;
+let index=1;
+
+prev.addEventListener("click", function(){
+    index--;
+    showItems();
+})
+next.addEventListener("click",function(){
+    index ++;
+    showItems()
+})
+
+function showItems(){
+    for(let i=0;i<firstSection.length;i++){
+        firstSection[i].classList.remove("show");
+        firstSection[i].classList.add("hide");
+
+
+        if(i>=(index*maxItem)-maxItem && i<index*maxItem){
+            firstSection[i].classList.add("show");
+            firstSection[i].classList.remove("hide");
+        }
+    }
+}
+
+window.onload=showItems();
