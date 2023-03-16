061ab1d9e9b7e042c8a78f2a15891bf40cd8c3a9
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-16 20:54:22 +0400

    design change

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-const list_items = [
-  "item 1",
-  "item 2",
-  "item 3",
-  "item 4",
-  "item 5",
-  "item 6",
-  "item 7",
-  "item 8",
-  "item 9",
-  "item 10",
-  "item 11",
-  "item 12",
-  "item 13",
-  "item 14",
-  "item 15",
-  "item 16",
-  "item 17",
-  "item 18",
-  "item 19",
-  "item 20",
-  "item 21",
-  "item 22",
-  "item 23",
-  "item 24",
-];
+// const list_items = [
+//   "item 1",
+//   "item 2",
+//   "item 3",
+//   "item 4",
+//   "item 5",
+//   "item 6",
+//   "item 7",
+//   "item 8",
+//   "item 9",
+//   "item 10",
+//   "item 11",
+//   "item 12",
+//   "item 13",
+//   "item 14",
+//   "item 15",
+//   "item 16",
+//   "item 17",
+//   "item 18",
+//   "item 19",
+//   "item 20",
+//   "item 21",
+//   "item 22",
+//   "item 23",
+//   "item 24",
+// ];
 
-const list_element = document.getElementById("list");
-const pagenumbers = document.getElementById("pagination");
+// const list_element = document.getElementById("list");
+// const pagenumbers = document.getElementById("pagination");
 
-let current_page = 1;
-let rows = 12;
+// let current_page = 1;
+// let rows = 12;
 
-function displayList(items, wrapper, rows_per_page, page) {
-  wrapper.innerHTML = "";
-  page--;
-  let start = rows_per_page * page;
-  let end = start + rows_per_page;
-  let paginatedItems = items.slice(start, end);
+// function displayList(items, wrapper, rows_per_page, page) {
+//   wrapper.innerHTML = "";
+//   page--;
+//   let start = rows_per_page * page;
+//   let end = start + rows_per_page;
+//   let paginatedItems = items.slice(start, end);
 
-  for (let i = 0; i < paginatedItems.length; i++) {
-    let item = paginatedItems[i];
-    let item_element = document.createElement("div");
-    item_element.classList.add("item");
-    item_element.innerText = item;
-    wrapper.appendChild(item_element);
-  }
-}
+//   for (let i = 0; i < paginatedItems.length; i++) {
+//     let item = paginatedItems[i];
+//     let item_element = document.createElement("div");
+//     item_element.classList.add("item");
+//     item_element.innerText = item;
+//     wrapper.appendChild(item_element);
+//   }
+// }
 
-function setupPagination(items, wrapper, rows_per_page) {
-  wrapper.innerHTML = "";
-  let page_count = math.ceil(items.length / wors_per_page);
-  for (let i = 1; i < page_count + 1; i++) {
-    let btn = paginationButton(i);
-    wrapper.appendChild(btn);
-  }
-}
+// function setupPagination(items, wrapper, rows_per_page) {
+//   wrapper.innerHTML = "";
+//   let page_count = math.ceil(items.length / wors_per_page);
+//   for (let i = 1; i < page_count + 1; i++) {
+//     let btn = paginationButton(i);
+//     wrapper.appendChild(btn);
+//   }
+// }
 
-function paginationButton(page) {
-  let button = document.createElement("button");
-  button.innerText = page;
-  if (current_page == page) button.classList.add("active");
-  return button;
-}
+// function paginationButton(page) {
+//   let button = document.createElement("button");
+//   button.innerText = page;
+//   if (current_page == page) button.classList.add("active");
+//   return button;
+// }
 
-displayList(list_items, list_element, rows, current_page);
-setupPagination(list_items, pagination_element, rows);
+// displayList(list_items, list_element, rows, current_page);
+// setupPagination(list_items, pagination_element, rows);
