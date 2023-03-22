725609802ee88e744778e90195c33741b8d90e13
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-22 16:57:11 +0400

    blog page is over
    need to change colors

--- script1.js
+++ script1.js
@@ -1,30 +1,31 @@
 const firstSection = document.querySelector(".cards").children;
 // console.log(firstSection)
-const prev =  document.querySelector(".prev");
-const next =  document.querySelector(".prev");
-const maxItem = 12;
-let index=1;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const maxItem = 9;
+let index = 1;
 
-prev.addEventListener("click", function(){
-    index--;
-    showItems();
-})
-next.addEventListener("click",function(){
-    index ++;
-    showItems()
-})
+prev.addEventListener("click", function () {
+  index--;
+  showItems();
+});
+next.addEventListener("click", function () {
+  index++;
+  showItems();
+});
 
-function showItems(){
-    for(let i=0;i<firstSection.length;i++){
-        firstSection[i].classList.remove("show");
-        firstSection[i].classList.add("hide");
+function showItems() {
+  for (let i = 0; i < firstSection.length; i++) {
+    firstSection[i].classList.remove("show");
+    firstSection[i].classList.add("hide");
 
-
-        if(i>=(index*maxItem)-maxItem && i<index*maxItem){
-            firstSection[i].classList.add("show");
-            firstSection[i].classList.remove("hide");
-        }
+    if (i >= index * maxItem - maxItem && i < index * maxItem) {
+      firstSection[i].classList.add("show");
+      firstSection[i].classList.remove("hide");
     }
+  }
 }
 
-window.onload=showItems();
+window.onload = showItems();
+
+
