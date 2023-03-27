ba305a095919849fed78437159f29e13ba335dd4
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-27 19:09:01 +0400

    ready for pagination

--- script.js
+++ script.js
@@ -1,222 +1,267 @@
 const CARDS = [
   {
-   
     imgUrl: "images/image1.jpg",
-    title:'gaming post',
-    description: 'Big Bang in Gaming history',
-    description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-    img2Url:'images/littleface.svg'
-  }, 
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+    img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
   {
+    imgUrl: "images/image2.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
 
-      imgUrl: "images/image2.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-      
-    },
-  
-    {
-  
-      imgUrl: "images/image3.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-  
-    },
-  
-    {
-  
-      imgUrl: "images/image4.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-    
-    },
-  
-    {
-  
-      imgUrl: "images/image5.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-  
-    },
-  
-    {
-  
-      imgUrl: "images/image6.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-  
-    {
-  
-      imgUrl: "images/image7.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image8.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image9.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image10.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image11.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image12.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image13.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image14.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image15.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image16.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image17.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image18.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image19.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image20.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image21.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image22.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image23.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    },
-    
-    {
-  
-      imgUrl: "images/image24.jpg",
-      title:'gaming post',
-      description: 'Big Bang in Gaming history',
-      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
-     
-    }
-]
+  {
+    imgUrl: "images/image3.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
 
+  {
+    imgUrl: "images/image4.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
 
+  {
+    imgUrl: "images/image5.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
 
+  {
+    imgUrl: "images/image6.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image7.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image8.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image9.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image10.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image11.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image12.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image13.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image14.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image15.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image16.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image17.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image18.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image19.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image20.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image21.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image22.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image23.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+
+  {
+    imgUrl: "images/image24.jpg",
+    title: "gaming post",
+    description: "Big Bang in Gaming history",
+    description2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
+      img2Url: "images/littleface.svg",
+    name: "Walter Pinkman",
+    date: "24. nov 2020",
+  },
+];
 
 function main() {
   const wrapper = document.getElementById("cards");
@@ -233,12 +278,12 @@ function main() {
     img.classList.add("img");
     card.appendChild(img);
 
-    // create title 
+    // create title
     const title = document.createElement("div");
     title.classList.add("title");
     title.innerHTML = cardObj.title;
     card.appendChild(title);
-    
+
     // description
     const description = document.createElement("div");
     description.classList.add("description");
@@ -251,16 +296,26 @@ function main() {
     description2.innerHTML = cardObj.description2;
     card.appendChild(description2);
 
-
     //svg
     const img2 = document.createElement("img");
     img2.src = cardObj.img2Url;
     img2.classList.add("img2");
     card.appendChild(img2);
 
+    // name
 
-}
+    const name = document.createElement("div");
+    name.classList.add("name");
+    name.innerHTML = cardObj.name;
+    card.appendChild(name);
 
+    //date
+
+    const date = document.createElement("div");
+    date.classList.add("date");
+    date.innerHTML = cardObj.date;
+    card.appendChild(date);
+  }
 }
 
 main();
