26dec53e55c8d7c4bc7448cee60a04469f009160
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-28 01:17:43 +0400

    pagination

--- script.js
+++ script.js
@@ -1,321 +1,164 @@
-const CARDS = [
-  {
-    imgUrl: "images/image1.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-    img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-  {
-    imgUrl: "images/image2.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
 
-  {
-    imgUrl: "images/image3.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
 
-  {
-    imgUrl: "images/image4.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image5.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image6.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image7.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image8.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image9.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image10.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image11.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image12.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image13.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image14.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image15.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image16.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image17.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image18.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image19.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image20.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image21.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image22.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image23.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-
-  {
-    imgUrl: "images/image24.jpg",
-    title: "gaming post",
-    description: "Big Bang in Gaming history",
-    description2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..",
-      img2Url: "images/littleface.svg",
-    name: "Walter Pinkman",
-    date: "24. nov 2020",
-  },
-];
-
-function main() {
-  const wrapper = document.getElementById("cards");
-
-  for (let cardObj of CARDS) {
-    // create card
-    const card = document.createElement("div");
-    card.classList.add("card");
-    wrapper.appendChild(card);
-
-    //img
-    const img = document.createElement("img");
-    img.src = cardObj.imgUrl;
-    img.classList.add("img");
-    card.appendChild(img);
-
-    // create title
-    const title = document.createElement("div");
-    title.classList.add("title");
-    title.innerHTML = cardObj.title;
-    card.appendChild(title);
-
-    // description
-    const description = document.createElement("div");
-    description.classList.add("description");
-    description.innerHTML = cardObj.description;
-    card.appendChild(description);
-
-    //description2
-    const description2 = document.createElement("div");
-    description2.classList.add("description2");
-    description2.innerHTML = cardObj.description2;
-    card.appendChild(description2);
-
-    //svg
-    const img2 = document.createElement("img");
-    img2.src = cardObj.img2Url;
-    img2.classList.add("img2");
-    card.appendChild(img2);
-
-    // name
-
-    const name = document.createElement("div");
-    name.classList.add("name");
-    name.innerHTML = cardObj.name;
-    card.appendChild(name);
-
-    //date
-
-    const date = document.createElement("div");
-    date.classList.add("date");
-    date.innerHTML = cardObj.date;
-    card.appendChild(date);
-  }
+function setPage(pageNum) {
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set("page", pageNum);
+  urlParams.set("limit", limit);
+  window.history.pushState(null, '', `?${urlParams.toString()}`);
 }
-
-main();
+ 
+function getLimit() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const limit = urlParams.get("limit");
+  return Number(limit ?? 9);
+}
+ 
+function getPage() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = urlParams.get("page");
+  return Number(page ?? 1);
+}
+ 
+let currentPage = getPage()
+const limit = getLimit()
+ 
+/**
+ * 
+ * @returns { 
+ *  products:{
+ *    id: number,
+ *    title: string,
+ *    description: string,
+ *    price: number,
+ *    discountPercentage: number,
+ *    rating: number,
+ *    stock: number,
+ *    brand: string,
+ *    category: string,
+ *    thumbnail: string,
+ *    images: string[]
+ *  }[]
+ * total: number,
+ * skip: number,
+ * limit: number
+ * }
+ * @param { limit: number, skip: number } options 
+ * @returns 
+ */
+const fetchPosts = async (options = {}) => {
+  let { limit, skip } = options;
+  limit = limit || 9;
+  skip = skip || 0;
+ 
+  const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
+  const data = await response.json();
+  return data;
+};
+ 
+ 
+const drawCard = (cardObj, wrapper) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  wrapper.appendChild(card);
+ 
+  // img
+  const img = document.createElement("img");
+  img.src = cardObj.thumbnail;
+  img.classList.add("img");
+  card.appendChild(img);
+ 
+  // create title
+  const title = document.createElement("div");
+  title.classList.add("title");
+  title.innerHTML = cardObj.title;
+  card.appendChild(title);
+ 
+  // description
+  const description = document.createElement("div");
+  description.classList.add("description");
+  description.innerHTML = cardObj.description;
+  card.appendChild(description);
+ 
+  // //description2
+  // const description2 = document.createElement("div");
+  // description2.classList.add("description2");
+  // description2.innerHTML = cardObj.description; // ToDo: description2 not provided in api
+  // card.appendChild(description2);
+ 
+  // //svg
+  // const img2 = document.createElement("img");
+  // img2.src = cardObj.images[0]; // ToDo: img2 not provided in api
+  // img2.classList.add("img2");
+  // card.appendChild(img2);
+ 
+  // // name
+  // const name = document.createElement("div");
+  // name.classList.add("name");
+  // name.innerHTML = cardObj.name; // ToDo: name not provided in api
+  // card.appendChild(name);
+ 
+  // //date
+  // const date = document.createElement("div");
+  // date.classList.add("date");
+  // date.innerHTML = cardObj.date; // ToDo: date not provided in api
+  // card.appendChild(date);
+};
+ 
+async function drawCards() {
+  const wrapper = document.getElementById("cards");
+  wrapper.innerHTML = "";
+  const skip = calculateCurrentSkip(currentPage, limit);
+  const result = await fetchPosts({ limit: limit, skip: skip });
+  setPage(currentPage)
+  for (let cardObj of result.products) {
+    drawCard(cardObj, wrapper);
+  }
+  validatePaginationButtons(result)
+}
+ 
+function calculateCurrentSkip(currentPage, limit) {
+  if (currentPage === 1) return 0;
+  return (currentPage - 1) * limit;
+}
+ 
+ 
+// listen for button click for next page
+document.getElementsByClassName("next")[0].addEventListener("click", () => {
+  currentPage += 1;
+  drawCards(currentPage);
+});
+ 
+document.getElementsByClassName("prev")[0].addEventListener("click", () => {
+  currentPage -= 1;
+  drawCards(currentPage);
+});
+ 
+function validatePaginationButtons(result) {
+  // disable next button if current page is last page
+  const nextPageButton = document.getElementsByClassName("next")[0];
+  const prevPageButton = document.getElementsByClassName("prev")[0];
+ 
+  if (currentPage === 1) {
+    disableButton(prevPageButton)
+  } else {
+    enableButton(prevPageButton)
+  }
+ 
+  if (result.total === result.skip + result.limit) {
+    disableButton(nextPageButton)
+  } else {
+    enableButton(nextPageButton)
+  }
+ 
+}
+ 
+function disableButton(elem) {
+  elem.style.pointerEvents = "none";
+}
+ 
+function enableButton(elem) {
+  elem.style.pointerEvents = "auto";
+ 
+}
+ 
+drawCards()
