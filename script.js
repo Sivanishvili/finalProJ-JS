546d8e706daa40db31fdd255284773bc5730907f
Sivanishvili <120643809+Sivanishvili@users.noreply.github.com>
2023-03-27 18:29:19 +0400

    ...

---
+++ script.js
@@ -0,0 +1,266 @@
+const CARDS = [
+  {
+   
+    imgUrl: "images/image1.jpg",
+    title:'gaming post',
+    description: 'Big Bang in Gaming history',
+    description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+    img2Url:'images/littleface.svg'
+  }, 
+  {
+
+      imgUrl: "images/image2.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+      
+    },
+  
+    {
+  
+      imgUrl: "images/image3.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+  
+    },
+  
+    {
+  
+      imgUrl: "images/image4.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+    
+    },
+  
+    {
+  
+      imgUrl: "images/image5.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+  
+    },
+  
+    {
+  
+      imgUrl: "images/image6.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+  
+    {
+  
+      imgUrl: "images/image7.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image8.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image9.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image10.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image11.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image12.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image13.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image14.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image15.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image16.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image17.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image18.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image19.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image20.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image21.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image22.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image23.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    },
+    
+    {
+  
+      imgUrl: "images/image24.jpg",
+      title:'gaming post',
+      description: 'Big Bang in Gaming history',
+      description2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique id neque, risus diam, nunc. Sit cras sed amet ..',
+     
+    }
+]
+
+
+
+
+function main() {
+  const wrapper = document.getElementById("cards");
+
+  for (let cardObj of CARDS) {
+    // create card
+    const card = document.createElement("div");
+    card.classList.add("card");
+    wrapper.appendChild(card);
+
+    //img
+    const img = document.createElement("img");
+    img.src = cardObj.imgUrl;
+    img.classList.add("img");
+    card.appendChild(img);
+
+    // create title 
+    const title = document.createElement("div");
+    title.classList.add("title");
+    title.innerHTML = cardObj.title;
+    card.appendChild(title);
+    
+    // description
+    const description = document.createElement("div");
+    description.classList.add("description");
+    description.innerHTML = cardObj.description;
+    card.appendChild(description);
+
+    //description2
+    const description2 = document.createElement("div");
+    description2.classList.add("description2");
+    description2.innerHTML = cardObj.description2;
+    card.appendChild(description2);
+
+
+    //svg
+    const img2 = document.createElement("img");
+    img2.src = cardObj.img2Url;
+    img2.classList.add("img2");
+    card.appendChild(img2);
+
+
+}
+
+}
+
+main();
